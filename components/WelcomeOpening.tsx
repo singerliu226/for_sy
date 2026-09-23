7a72fbcbb6f9e93a10b2ab57b7c2622fbba27e14
singerliu226 <singer226@163.com>
2026-09-23 14:18:43 +0800

    Serve Mid-Autumn welcome video statically

--- components/WelcomeOpening.tsx
+++ components/WelcomeOpening.tsx
@@ -75,7 +75,7 @@ export function WelcomeOpening() {
             <i className="welcome-video__moon" /><i className="welcome-video__lantern welcome-video__lantern--left" /><i className="welcome-video__lantern welcome-video__lantern--right" />
             <p className="welcome-video__title">中秋回家</p>
             <div className="welcome-video__frame">
-              <video ref={video} src="/api/welcome-video" autoPlay muted playsInline preload="auto" onCanPlay={tryVideo} onPlaying={() => setVideoPlaying(true)} onEnded={dismiss} onError={() => setVideoFailed(true)} />
+              <video ref={video} src="/welcome/midautumn-homecoming.mp4" autoPlay muted playsInline preload="auto" onCanPlay={tryVideo} onPlaying={() => setVideoPlaying(true)} onEnded={dismiss} onError={() => setVideoFailed(true)} />
               <span>思怡，欢迎回家！</span>
             </div>
           </div>}
