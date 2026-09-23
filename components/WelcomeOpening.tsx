6b82e8c4e91a648365398fca1a7c90a092450044
singerliu226 <singer226@163.com>
2026-09-23 14:26:40 +0800

    Use resilient welcome media route

--- components/WelcomeOpening.tsx
+++ components/WelcomeOpening.tsx
@@ -78,7 +78,7 @@ export function WelcomeOpening() {
             <i className="welcome-video__moon" /><i className="welcome-video__lantern welcome-video__lantern--left" /><i className="welcome-video__lantern welcome-video__lantern--right" />
             <p className="welcome-video__title">中秋回家</p>
             <div className="welcome-video__frame">
-              <video ref={video} src="/welcome/midautumn-homecoming-mobile.mp4" autoPlay defaultMuted muted playsInline preload="auto" onCanPlay={tryVideo} onPlaying={() => setVideoPlaying(true)} onEnded={dismiss} onError={() => setVideoFailed(true)} />
+              <video ref={video} src="/welcome/midautumn-homecoming" autoPlay defaultMuted muted playsInline preload="auto" onCanPlay={tryVideo} onPlaying={() => setVideoPlaying(true)} onEnded={dismiss} onError={() => setVideoFailed(true)} />
               <span>思怡，欢迎回家！</span>
             </div>
           </div>}
